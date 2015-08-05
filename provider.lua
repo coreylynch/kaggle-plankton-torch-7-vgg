ad7279408000013dea896113dc139a4480f02d6b
Corey Lynch <coreylynch9@gmail.com>
2015-08-05 14:11:18 -0400

    first post easily claimed

---
+++ provider.lua
@@ -0,0 +1,88 @@
+require 'nn'
+require 'image'
+require 'xlua'
+require 'paths'
+
+local Provider = torch.class 'Provider'
+
+function Provider:__init(full)
+  local total_training_size = 30336
+  local train_size = torch.floor(total_training_size * 0.8)
+  local validation_size = total_training_size - train_size
+  local test_size = 130400
+  local resize = 32
+
+  local data = torch.Tensor(total_training_size, 1, resize, resize):zero():float()
+  local labels = torch.Tensor(total_training_size)
+
+  local labels_to_index = {}
+  local index_to_labels = {}
+
+  local rorder = torch.randperm(total_training_size)
+
+  local dir_idx = 0
+  local i = 0
+
+  print("Loading and resizing training")
+  for d in paths.iterdirs('train') do
+    xlua.progress(i, total_training_size)
+    dir_idx = dir_idx + 1
+    labels_to_index[d] = dir_idx
+    index_to_labels[dir_idx] = d
+    for f in paths.iterfiles('train/'..d) do
+       i = i + 1
+       im = image.load('train/'..d..'/'..f)
+       data[rorder[i]] = image.scale(im, resize, resize)
+       labels[rorder[i]] = labels_to_index[d]
+    end
+  end
+
+  -- Load training / validation data 
+  self.trainData = {
+     data = data[{{1,train_size},{},{},{}}],
+     labels = labels[{{1,train_size}}],
+     size = function() return train_size end
+  }
+  local trainData = self.trainData
+
+  self.validData = {
+     data = data[{{train_size+1, total_training_size},{},{},{}}],
+     labels = labels[{{train_size+1, total_training_size}}],
+     size = function() return (validation_size) end
+  }
+  local validData = self.validData
+
+  -- Load test data
+  i = 0
+  print("Loading and resizing test")
+  test_data = torch.Tensor(test_size, 1, resize, resize):zero()
+  for file in paths.iterfiles('test/') do
+     xlua.progress(i, test_size)
+     i = i + 1
+     im = image.load('test/'..file)
+     test_data[i] = image.scale(im, resize, resize)
+  end
+
+  self.testData = {
+     data = test_data,
+     size = function() return test_size end
+  }
+  local testData = self.testData
+ end
+
+
+function Provider:normalize()
+  local trainData = self.trainData
+  local testData = self.testData
+
+  local mean = trainData.data:select(2,1):mean()
+  local std = trainData.data:select(2,1):std()
+  trainData.data:select(2,1):add(-mean)
+  trainData.data:select(2,1):div(std)
+  trainData.mean = mean
+  trainData.std = std
+
+  testData.data:select(2,1):add(-mean)
+  testData.data:select(2,1):div(std)
+end
+
---
+++ train.lua
@@ -0,0 +1,182 @@
+require 'xlua'
+require 'optim'
+require 'cutorch'
+dofile './provider.lua'
+local c = require 'trepl.colorize'
+
+opt = lapp[[
+   -s,--save                  (default "logs")      subdirectory to save logs
+   -b,--batchSize             (default 128)          batch size
+   -r,--learningRate          (default 1)        learning rate
+   --learningRateDecay        (default 1e-7)      learning rate decay
+   --weightDecay              (default 0.0005)      weightDecay
+   -m,--momentum              (default 0.9)         momentum
+   --epoch_step               (default 25)          epoch step
+   --model                    (default vgg_bn_drop)     model name
+   --max_epoch                (default 300)           maximum number of iterations
+]]
+
+print(opt)
+
+do -- data augmentation module
+  local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
+
+  function BatchFlip:__init()
+    parent.__init(self)
+    self.train = true
+  end
+
+  function BatchFlip:updateOutput(input)
+    if self.train then
+      local bs = input:size(1)
+      local flip_mask = torch.randperm(bs):le(bs/2)
+      for i=1,input:size(1) do
+        if flip_mask[i] == 1 then image.hflip(input[i], input[i]) end
+      end
+    end
+    self.output = input
+    return self.output
+  end
+end
+
+print(c.blue '==>' ..' configuring model')
+local model = nn.Sequential()
+model:add(nn.BatchFlip():float())
+model:add(nn.Copy('torch.FloatTensor','torch.CudaTensor'):cuda())
+model:add(nn.Sequential():add(dofile('models/'..opt.model..'.lua'):cuda()))
+model:get(2).updateGradInput = function(input) return end
+print(model)
+
+print(c.blue '==>' ..' loading data')
+provider = torch.load 'provider.t7'
+
+confusion = optim.ConfusionMatrix(121)
+
+print('Will save at '..opt.save)
+paths.mkdir(opt.save)
+testLogger = optim.Logger(paths.concat(opt.save, 'test.log'))
+testLogger:setNames{'% mean class accuracy (train set)', '% mean class accuracy (test set)'}
+
+parameters,gradParameters = model:getParameters()
+
+print(c.blue'==>' ..' setting criterion')
+criterion = nn.CrossEntropyCriterion():cuda()
+
+
+print(c.blue'==>' ..' configuring optimizer')
+optimState = {
+  learningRate = opt.learningRate,
+  weightDecay = opt.weightDecay,
+  momentum = opt.momentum,
+  learningRateDecay = opt.learningRateDecay,
+}
+optimMethod = optim.sgd
+
+
+function train()
+  model:training()
+  epoch = epoch or 1
+
+  -- drop learning rate every "epoch_step" epochs
+  if epoch % opt.epoch_step == 0 then optimState.learningRate = optimState.learningRate/2 end
+  
+  print(c.blue '==>'.." online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
+
+  local targets = torch.CudaTensor(opt.batchSize)
+  local indices = torch.randperm(provider.trainData.data:size(1)):long():split(opt.batchSize)
+  -- remove last element so that all the batches have equal size
+  indices[#indices] = nil
+
+  local tic = torch.tic()
+  for t,v in ipairs(indices) do
+    xlua.progress(t, #indices)
+
+    local inputs = provider.trainData.data:index(1,v)
+    targets:copy(provider.trainData.labels:index(1,v))
+
+    local feval = function(x)
+      if x ~= parameters then parameters:copy(x) end
+      gradParameters:zero()
+      
+      local outputs = model:forward(inputs)
+      local f = criterion:forward(outputs, targets)
+      local df_do = criterion:backward(outputs, targets)
+      model:backward(inputs, df_do)
+
+      confusion:batchAdd(outputs, targets)
+
+      return f,gradParameters
+    end
+    optim.sgd(feval, parameters, optimState)
+  end
+
+  confusion:updateValids()
+  print(('Train accuracy: '..c.cyan'%.2f'..' %%\t time: %.2f s'):format(
+        confusion.totalValid * 100, torch.toc(tic)))
+
+  train_acc = confusion.totalValid * 100
+
+  confusion:zero()
+  epoch = epoch + 1
+end
+
+
+function test()
+  -- disable flips, dropouts and batch normalization
+  model:evaluate()
+  print(c.blue '==>'.." testing")
+  local n = provider.validData.data:size(1)
+  local bs = 125
+  for i=1,n,bs do
+    _,y = model:forward(provider.validData.data:narrow(1,i,bs)):max(2)
+    -- confusion:batchAdd(y,provider.validData.labels:narrow(1,i,bs))
+  end
+
+  confusion:updateValids()
+  print('Test accuracy:', confusion.totalValid * 100)
+  
+  if testLogger then
+    paths.mkdir(opt.save)
+    testLogger:add{train_acc, confusion.totalValid * 100}
+    testLogger:style{'-','-'}
+    testLogger:plot()
+    local file = io.open(opt.save..'/report.html','w')
+    local header = [[
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <title>$SAVE - $EPOCH</title>
+    <img src="test.png">
+    ]]
+    header = header:gsub('$SAVE',opt.save):gsub('$EPOCH',epoch)
+    file:write(header)
+    file:write'<h4>optimState:</h4>\n'
+    file:write'<table>\n'
+    for k,v in pairs(optimState) do
+      if torch.type(v) == 'number' then
+        file:write('<tr><td>'..k..'</td><td>'..v..'</td></tr>\n')
+      end
+    end
+    file:write'</table><pre>\n'
+    file:write(tostring(confusion)..'\n')
+    file:write(tostring(model)..'\n')
+    file:write("</pre></body></html>")
+    file:close()
+    os.execute('convert -density 200 '..opt.save..'/test.log.eps '..opt.save..'/test.png')
+  end
+
+  -- save model every 50 epochs
+  if epoch % 50 == 0 then
+    local filename = paths.concat(opt.save, 'model.net')
+    print('==> saving model to '..filename)
+    torch.save(filename, model:get(3))
+  end
+
+  confusion:zero()
+end
+
+
+for i=1,opt.max_epoch do
+  train()
+  test()
+end
